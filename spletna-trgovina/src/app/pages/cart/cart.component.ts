c3b6dc2ad7e00dad3247aeccfb0d0bfd4ba069a0
Aljoša Vister <aljosa.vister@gmail.com>
2025-05-05 19:16:18 +0200

    Basic cart

--- spletna-trgovina/src/app/pages/cart/cart.component.ts
+++ spletna-trgovina/src/app/pages/cart/cart.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
+import { AsyncPipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Izdelek } from '../shop/products';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, AsyncPipe, MatCardModule, MatButtonModule],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+  constructor(public cartService: CartService) {}
 
+  getSkupnaCena(artikli: Izdelek[]): number {
+    return artikli.reduce((vsota, izdelek) => vsota + izdelek.cena, 0);
+  }
+
+  zakljuciNakup() {
+    alert('Hvala za nakup! (simulirano)');
+    this.cartService.clearCart();
+  }
 }
