7cd8311463ce6f19d0c7505e9dc78e017d934e61
Aljoša Vister <aljosa.vister@gmail.com>
2025-05-10 07:46:15 +0200

    Added contacts form

--- spletna-trgovina/src/app/pages/contact/contact.component.ts
+++ spletna-trgovina/src/app/pages/contact/contact.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      ime: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      sporocilo: ['', Validators.required]
+    });
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log('Kontaktno sporočilo:', this.form.value);
+      alert('Hvala za sporočilo! (simulirano)');
+      this.form.reset();
+    }
+  }
 }
