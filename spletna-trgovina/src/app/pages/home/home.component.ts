698f7c8b278ab927271a6f14224b21e47a44c055
Aljosa Vister <aljosa.vister@gmail.com>
2025-05-13 07:50:35 +0200

    Se nekaj posodobitev

--- spletna-trgovina/src/app/pages/home/home.component.ts
+++ spletna-trgovina/src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
